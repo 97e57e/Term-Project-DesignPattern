362b17d9273a56baf647e8bc4b83ed283e8d71c6
gardenLee <khjvvv7@gmail.com>
2019-11-21 23:07:50 +0900

    Class, Id로 div 태그 찾기 구현

--- src/main/java/org/jsoup/userInterface/UserInterface.java
+++ src/main/java/org/jsoup/userInterface/UserInterface.java
@@ -12,8 +12,7 @@ public class UserInterface {
 	Scanner scan;
 	
 	private String address;
-	private String className;
-	private String idName;
+	private String inputName;
 	
 	boolean isFinish;
 	int run_code;
@@ -38,33 +37,45 @@ public class UserInterface {
 			System.out.println(" *** Jsoup Interface ***");
 			System.out.print("주소를 입력 해 주세요 : ");
 			address = scan.nextLine();
-			System.out.println(address + "를 파싱합니다.\n");
-			/*문서 가져오기*/
-			try {
-			doc = Jsoup.connect(address).get();
-//			Elements els = doc.select(".header").select(".nav-sections");
-			System.out.println("문서 가져오기 완료!");
-			}catch(Exception e) {}
 			
-			System.out.println(" *** 무엇을 할까요? ***");
-			System.out.println("1. Search by ClassName");
-			System.out.println("2. Search by Id");
-			System.out.println("0. 끝내기");
-			run_code = scan.nextInt();
-			if(run_code == 0 ) break;
+			System.out.println(address + "를 가져옵니다.\n");
 			
-			
-			switch (run_code) {
-			case 1:
-				className = scan.nextLine();
-				className = "." + className;
-				System.out.println(className + "가지고 찾아보자!");
-				break;
-			case 2:
-				idName = scan.nextLine();
-				break;
+			while(true) {
+				/*문서 가져오기*/
+				try {
+				doc = Jsoup.connect(address).get();
+				}catch(Exception e) {}
+				
+				System.out.println(" *** 무엇을 할까요? ***");
+				System.out.println("1. Search by ClassName");
+				System.out.println("2. Search by Id");
+				System.out.println("3. 다른 주소 입력");
+				System.out.println("0. 끝내기");
+				run_code = scan.nextInt();
+				scan.nextLine(); // 개행문자 제거
+				if(run_code == 0 || run_code == 3) break;
+				
+				
+				switch (run_code) {
+				case 1:
+					System.out.print("찾고자 하는 Class 이름을 입력 해 주세요 : ");
+					inputName = scan.nextLine();
+					inputName = "." + inputName;
+					break;
+				case 2:
+					System.out.print("찾고자 하는 ID를 입력 해 주세요 : ");
+					inputName = scan.nextLine();
+					inputName = "#" + inputName;
+					break;
+				}
+				Elements els = doc.select(inputName);
+				if (els == null) {
+					System.out.println("결과가 없습니다.");
+				} else {
+					System.out.println(els);
+				}
 			}
-			
+			if (run_code ==0) isFinish=true;
 		}
 	}
 	public void setAddress(String address) {
