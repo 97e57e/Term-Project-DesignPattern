76aca4c3277bc9df127d6be98aca4fa03c553d9d
Jaehooooooon <jaehoon572@naver.com>
2019-11-27 20:30:04 +0900

    [TraverDown] 예외처리

--- src/main/java/org/jsoup/userInterface/TraverseDown.java
+++ src/main/java/org/jsoup/userInterface/TraverseDown.java
@@ -5,6 +5,10 @@ import org.jsoup.nodes.Element;
 public class TraverseDown implements Traverse{
 	
 	public Element traverse(Element e) {
+		if (e.children().isEmpty()) {
+			System.out.println("더 이상 child가 없습니다.\n");
+			return e;
+		}
 		e = e.child(0);
 		System.out.println("( " + 1 + " / " + e.childNodeSize() + " )\n");
 
