2a2acd15da3f7900c73cbecc2bc195c5fd8e461f
Jaehooooooon <jaehoon572@naver.com>
2019-11-27 20:19:37 +0900

    [TraverseDown]

--- src/main/java/org/jsoup/userInterface/TraverseDown.java
+++ src/main/java/org/jsoup/userInterface/TraverseDown.java
@@ -5,6 +5,9 @@ import org.jsoup.nodes.Element;
 public class TraverseDown implements Traverse{
 	
 	public Element traverse(Element e) {
+		e = e.child(0);
+		System.out.println("( " + 1 + " / " + e.childNodeSize() + " )\n");
+
 		return e;
 	}
 }
--- src/main/java/org/test/MainClass.java
+++ src/main/java/org/test/MainClass.java
@@ -1,17 +1,23 @@
 package org.test;
 
-import org.jsoup.userInterface.UserInterface;
+import java.io.IOException;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.jsoup.userInterface.UserInterface;
 
 public class MainClass {
 
 	public static void main(String[] args) {
 		UserInterface userInterface;
-		
+
 		/* Singleton 객체인 UserInterface 객체 가져옴 */
 		userInterface = UserInterface.getUserInterface();
-		
+
 		/* 시작! */
 		userInterface.run();
+
 	}
 }
